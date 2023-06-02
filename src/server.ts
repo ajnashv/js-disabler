4f1d2fcd8c52b3a9870b9da6dabd829ba4ac46fd
barbarabrito <barbaramabrito@gmail.com>
2023-06-02 20:21:39 -0300

    remove args from puppeteer call

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ app.post('/submitUrl', urlencodedParser, function (req: Request, res: Response) 
 
 const browse = async (url: string) => {
 
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.setRequestInterception(true);
 
